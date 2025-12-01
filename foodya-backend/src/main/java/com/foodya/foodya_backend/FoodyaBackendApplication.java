bf25fd49c952616b5ea77594677c269cdf9c229f
nhatkhanhisme <hnk.uit.k18@gmail.com>
2025-12-01 18:12:49 +0700

    init repo

---
+++ foodya-backend/src/main/java/com/foodya/foodya_backend/FoodyaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodya.foodya_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodyaBackendApplication.class, args);
+	}
+
+}
---
+++ foodya-backend/src/test/java/com/foodya/foodya_backend/FoodyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodya.foodya_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
