0db18e2c402e0a0f22196b42a85aeb086ed5a179
Nhat Khanh Ho <hnk.uit.k18@gmail.com>
2026-07-21 08:22:10 +0700

    remove mobile folder

--- mobile/app/src/main/java/com/example/foodya/domain/model/CartItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.foodya.domain.model
-
-data class CartItem (
-    val menuItem: Food,
-    val quantity: Int
-)
--- mobile/app/src/main/java/com/example/foodya/util/Extensions.kt
+++
@@ -1,9 +0,0 @@
-package com.example.foodya.util
-
-import java.text.NumberFormat
-import java.util.Locale
-
-fun Double.toCurrency(): String {
-    val formatter = NumberFormat.getCurrencyInstance(Locale("vi", "VN"))
-    return formatter.format(this)
-}
